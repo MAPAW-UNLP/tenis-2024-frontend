137d17ee4fe9f0dd43d45d41aaa696f40a9a44a4
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-09-23 12:53:56 -0300

    agregar SessionContext, SessionProvider y useSession

---
+++ src/common/context/SessionContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+export const SessionContext = createContext(null)
+
+export function SessionProvider({children}) {
+    //para la sesion
+    const navigate = useNavigate();
+    const [session, setSession] = useState('');
+  
+    //sesion Effect D:
+    useEffect(() => {
+      const user = localStorage.getItem('session');
+      if (user === '') {
+        navigate('/');
+      }
+    }, [navigate, session]);
+
+    return (
+      <SessionContext.Provider value={{
+        session,
+        setSession
+      }}>
+        {children}
+      </SessionContext.Provider>
+    )
+}
---
+++ src/common/hooks/useSession.js
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { SessionContext } from "../context/SessionContext";
+
+export function useSession() {
+  return useContext(SessionContext)
+}
