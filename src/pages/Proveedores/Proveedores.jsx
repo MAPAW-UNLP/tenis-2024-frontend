470221f35efa92fb1d44294349c1945baf6681ff
pvalero <pablo.valero25@gmail.com>
2024-10-07 18:54:14 -0300

    Se agregaron mejoras para la pantalla de listado de proveedores

--- src/pages/Proveedores/Proveedores.jsx
+++ src/pages/Proveedores/Proveedores.jsx
@@ -9,21 +9,61 @@ function Proveedores() {
 
     const URL_BASE = `http://localhost:8083/api/`
 
-    const [proveedores, setProveedores] = useState([])
+    const CANT_FILAS = 5
 
+    const [proveedores, setProveedores] = useState([])
     const [mostrarPopup, setMostrarPopup] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [updateList, setUpdateList] = useState(false)
+    const [pagina, setPagina] = useState(0)
+
+    const totalDePaginas = proveedores.length / CANT_FILAS
 
     useEffect(() => {
+        setLoading(true)
         fetch(`${URL_BASE}proveedor`)
             .then((response) => response.json())
             .then((data) => {
                 setProveedores(data)
-            })
+                setLoading(false)
+            }).catch(() => setLoading(false))
         // .then(() => setAlumnosLoader(() => false))
-    }, [])
+    }, [updateList])
 
     const activarFormulario = () => { setMostrarPopup(true) }
     const ocultarFormulario = () => { setMostrarPopup(false) }
+
+    let listado;
+    const filtrarArray = (paginaActual) => {
+        listado = proveedores.slice(paginaActual * CANT_FILAS, (paginaActual + 1) * CANT_FILAS)
+    }
+    filtrarArray(pagina)
+
+    const update = () => {
+        setUpdateList(!updateList)
+    }
+
+    const atras = () => {
+        setPagina(prev => prev - 1)
+    }
+
+    const siguiente = () => {
+        setPagina(prev => prev + 1)
+    }
+
+    const enableBtnBack = () => {
+        if(pagina == 0){
+            return <button className='btnSelectPage' disabled onClick={atras}>Atrás</button>
+        }
+        return <button className='btnSelectPage' onClick={atras}>Atrás</button>
+    }
+
+    const enableBtnNext = () => {
+        if(pagina < Math.floor(totalDePaginas)){
+            return <button className='btnSelectPage' onClick={siguiente}>Siguiente</button>
+        }
+        return <button className='btnSelectPage' disabled onClick={siguiente}>Siguiente</button>
+    }
 
     return (
         <div id="proveedores-component">
@@ -46,26 +86,46 @@ function Proveedores() {
                 </div>
                 <div className="table-head-proveedores">
                     <span style={{ fontSize: '1.8em', width: 200, textAlign: "center" }}>Nombre</span>
-                    <span style={{ fontSize: '1.8em', width: 200, textAlign: "center" }}>Telefono</span>
+                    <span style={{ fontSize: '1.8em', width: 200, textAlign: "center" }}>Teléfono</span>
                     <span></span>
                 </div>
-                <div className="container-table-proveedores">
-                    {
-                        proveedores.map(p => {
-                            return <div key={p.id} className='proveedores-item-list'>
-                                <p>{p.nombre}</p>
-                                <p>{p.telefono}</p>
-                                <span></span>
+                {
+                    loading ? (
+                        <div className="container-table-proveedores" >
+                            <div className="spinner" style={{ position: "relative", marginTop: "10%" }}></div>
+                        </div>
+                    ) : (
+                        <>
+                            <div className="container-table-proveedores">
+                                {
+                                    listado.map(p => {
+                                        return <div key={p.id} className='proveedores-item-list'>
+                                            <p>{p.nombre}</p>
+                                            <p>{p.telefono}</p>
+                                            <span></span>
+                                        </div>
+                                    })
+                                }
                             </div>
-                        })
-                    }
-                </div>
+                            <div style={{display:"flex", gap:10}}>
+                                {
+                                    enableBtnBack()
+                                }
+                                {
+                                    enableBtnNext()
+                                }
+                            </div>
+                        </>
+                    )
+                }
+
+
                 {mostrarPopup && (
-                    <AgregarProveedor handleCloseForm={ocultarFormulario} proveedores={proveedores} />
+                    <AgregarProveedor handleCloseForm={ocultarFormulario} proveedores={proveedores} updateList={update} />
                 )}
             </div>
 
-        </div>
+        </div >
     )
 }
 
