f0b98713133c32dae341ab215e40c089887dc7ba
Geronimo Errante <geronimoerrante7@gmail.com>
2024-11-04 22:10:57 -0300

    descomento codigo

--- src/components/ProfileMenu/ProfileSideBar.jsx
+++ src/components/ProfileMenu/ProfileSideBar.jsx
@@ -82,7 +82,7 @@ const ProfileSideBar = () => {
           <FontAwesomeIcon icon={faTimes} />
         </button>
         <div className="profile-sidebar-container">
-          {/* <h2 className="h2-sidebar">Opciones del Perfil</h2>
+          <h2 className="h2-sidebar">Opciones del Perfil</h2>
           {session.roles.length > 1 && (
             <button
               className="list-toggle options-sidebar"
@@ -90,7 +90,7 @@ const ProfileSideBar = () => {
             >
               Roles
             </button>
-          )} */}
+          )}
 
           {expanded && session.roles.length > 1 && (
             <ul className="options-list">
