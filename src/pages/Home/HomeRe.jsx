d85fbbc8e7eec057e74d96f98f6def0053e5792e
Gianlucas <gianlucasgalati11@gmail.com>
2024-11-30 12:12:15 -0300

    dont show reserves and movements for client

--- src/pages/Home/HomeRe.jsx
+++ src/pages/Home/HomeRe.jsx
@@ -36,22 +36,26 @@ export const HomeRe = () => {
           gap: '1em',
         }}
       >
-        <Card
-          title="movimientos"
-          description="Movimiento de dinero y manejo de cobros"
-          customColor="#94f5c5"
-          icon={faDollarSign}
-          className="movimientosCard"
-        />
+        {(user.rolPorDefecto === 'ROLE_ADMIN' ||
+          user.rolPorDefecto === 'ROLE_PROFESOR') && (
+          <>
+            <Card
+              title="movimientos"
+              description="Movimiento de dinero y manejo de cobros"
+              customColor="#94f5c5"
+              icon={faDollarSign}
+              className="movimientosCard"
+            />
 
-        <Card
-          title="reservas"
-          description="Reservación de alquileres y clases"
-          customColor="#ffa500"
-          icon={faCalendar}
-          className="reservasCard"
-        />
-
+            <Card
+              title="reservas"
+              description="Reservación de alquileres y clases"
+              customColor="#ffa500"
+              icon={faCalendar}
+              className="reservasCard"
+            />
+          </>
+        )}
         {user.rolPorDefecto === 'ROLE_CLIENTE' && (
           <>
             <Card
