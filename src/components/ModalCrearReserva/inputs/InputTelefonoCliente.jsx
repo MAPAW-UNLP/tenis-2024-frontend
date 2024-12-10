2ac3244a698cb71390a0e915b72a611d9ef650b9
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-10 12:55:47 -0300

    telefono solo permite ingresar numeros

--- src/components/ModalCrearReserva/inputs/InputTelefonoCliente.jsx
+++ src/components/ModalCrearReserva/inputs/InputTelefonoCliente.jsx
@@ -3,6 +3,12 @@ import { useCrearReserva } from '../context/useCrearReserva'
 export default function InputTelefonoCliente() {
   const { telefonoCliente, updateField } = useCrearReserva()
 
+  function handleChange(e) {
+    if (/^\d*$/.test(e.target.value)) {
+      updateField('telefonoCliente', e.target.value)
+    }
+  }
+
   return (
     <div className="reserva-group">
       <label htmlFor="telefonoCliente" className="reserva-group__label">
@@ -13,9 +19,10 @@ export default function InputTelefonoCliente() {
         id="telefonoCliente"
         name="telefonoCliente"
         type="text"
+        inputMode="numeric"
         placeholder="Teléfono del cliente"
         value={telefonoCliente.value}
-        onChange={(e) => updateField('telefonoCliente', e.target.value)}
+        onChange={handleChange}
       />
     </div>
   )
